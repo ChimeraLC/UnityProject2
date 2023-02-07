5a70c277851de36fbda7d62bb2e34c9f77c9efd9
ChimeraLC <lucaschen314@gmail.com>
2023-02-06 22:54:28 -0600

    inheritance hierarchy setup

--- Assets/Scripts/PlayerProjectile1Controller.cs
+++ Assets/Scripts/PlayerProjectile1Controller.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProjectile1Controller : MonoBehaviour
+public class PlayerProjectile1Controller : PlayerProjectileInterface
 {
-    private Rigidbody2D projectileRb;
-    private BoxCollider2D projectileBc;
-    private Vector2 mousePosition;
-    private float initialSpeed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        //initial movement
-        projectileBc = GetComponent<BoxCollider2D>();
-        projectileRb = GetComponent<Rigidbody2D>();
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        projectileRb.AddForce((mousePosition - (Vector2)transform.position).normalized * initialSpeed, ForceMode2D.Impulse);
+        base.initialize();
     }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -24,10 +19,5 @@ public class PlayerProjectile1Controller : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("hello");
-        //colliding with walls
-        if (collision.gameObject.CompareTag("Wall")) Destroy(this.gameObject);
-    }
+
 }
---
+++ Assets/Scripts/PlayerProjectileInterface.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerProjectileInterface : MonoBehaviour
+{
+    protected Rigidbody2D projectileRb;
+    protected BoxCollider2D projectileBc;
+    protected Vector2 mousePosition;
+    protected float initialSpeed = 5.0f;
+    protected Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    //runs start method for heirarchy
+    protected void initialize()
+    {
+        //initial movement
+        projectileBc = GetComponent<BoxCollider2D>();
+        projectileRb = GetComponent<Rigidbody2D>();
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - (Vector2)transform.position).normalized;
+        projectileRb.AddForce(direction * initialSpeed, ForceMode2D.Impulse);
+
+    }
+
+    //return the direction the projectile is going
+    public Vector2 getDirection()
+    {
+        return direction;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        //colliding with walls
+        if (collision.gameObject.CompareTag("Wall")) Destroy(this.gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        direction = projectileRb.velocity.normalized;
+    }
+}
