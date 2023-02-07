eda869eb3bb732bb4e76e0fcf77a4d52e8f6a035
ChimeraLC <lucaschen314@gmail.com>
2023-02-06 22:54:12 -0600

    enemy behavior change

--- Assets/Scripts/EnemyScripts/SlimeController.cs
+++ Assets/Scripts/EnemyScripts/SlimeController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SlimeController : MonoBehaviour
@@ -9,8 +10,12 @@ public class SlimeController : MonoBehaviour
     private Vector2 playerDirection;
     private Rigidbody2D enemyRb;
     private SlimeEyeController eye;
+    private Animator enemyAnim;
+    private int enemyHp;
+    private int state;
     void Start()
     {
+        enemyAnim = GetComponent<Animator>();
         enemyRb = GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player");
         eye = transform.GetChild(0).GetComponent<SlimeEyeController>();
@@ -22,12 +27,38 @@ public class SlimeController : MonoBehaviour
         //find player location
         playerDirection = (player.transform.position - transform.position).normalized;
 
-        //move towards player
+        /* States:
+         * 0 - chase
+         * 1 - hurt
+         */
+        if (state == 0)
+        {
+            //move towards player
+            enemyRb.velocity = playerDirection;
 
-        enemyRb.velocity = playerDirection;
+            //update eye position
+            eye.Look(playerDirection);
+        }
+    }
+    //collision detection
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //collision with player projectile
+        if (collision.gameObject.CompareTag("PlayerProjectile"))
+        {
+            enemyRb.velocity = collision.gameObject.GetComponent<PlayerProjectile1Controller>().getDirection();
+            Destroy(collision.gameObject);
+            enemyAnim.SetInteger("state", 1);
+            state = 1;
+            //return to chase state after a certain time
+            StartCoroutine(HurtTimer(1f));
+        }
+    }
 
-        //update eye position
-        eye.Look(playerDirection);
-
+    public IEnumerator HurtTimer(float t)
+    {
+        yield return new WaitForSeconds(t);
+        state = 0;
+        enemyAnim.SetInteger("state", 0);
     }
 }
